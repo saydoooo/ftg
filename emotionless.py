9c081fbac35768a96d0f3b4de920c3a2864aef22
hikari <me@hikariatama.ru>
2022-03-14 06:14:54 +0000

    asdfasdfasdfasdfasdfasdfadsf

--- emotionless.py
+++ emotionless.py
@@ -14,6 +14,7 @@
 
 # meta pic: https://img.icons8.com/external-vitaliy-gorbachev-flat-vitaly-gorbachev/58/000000/external-sad-social-media-vitaliy-gorbachev-flat-vitaly-gorbachev.png
 # scope: geektg_min 3.1.17
+# scope: geektg_only
 # requires: telethon-mod
 
 from .. import loader, utils
@@ -21,7 +22,10 @@ from telethon.tl.types import Message
 from telethon.events import Raw
 from telethon.tl.types import UpdateMessageReactions
 from telethon.tl.functions.messages import ReadReactionsRequest
+from telethon.utils import get_input_peer
 import logging
+import asyncio
+import time
 
 logger = logging.getLogger(__name__)
 
@@ -41,12 +45,18 @@ class EmotionlessMod(loader.Module):
     async def client_ready(self, client, db) -> None:
         self._db = db
         self._client = client
+        self._flood_protect = []
+        self._queue = {}
+        self._flood_protect_sample = 60
+        self._threshold = 10
 
         self.handler = (self._handler, Raw)
         client.add_event_handler(*self.handler)
+        self._task = asyncio.ensure_future(self._queue_handler())
 
     async def on_unload(self) -> None:
         self._client.remove_event_handler(*self.handler)
+        self._task.cancel()
 
     async def noreactscmd(self, message: Message) -> None:
         """Toggle reactions auto-reader"""
@@ -56,20 +66,43 @@ class EmotionlessMod(loader.Module):
             message, self.strings("state").format("on" if state else "off")
         )
 
+    async def _queue_handler(self) -> None:
+        while True:
+
+            for chat, schedule in self._queue.copy().items():
+                if schedule < time.time():
+                    await self._client(ReadReactionsRequest(get_input_peer(chat)))
+                    logger.debug(f"Read reactions in queued peer {chat}")
+                    del self._queue[chat]
+
+            await asyncio.sleep(5)
+
     async def _handler(self, event: Raw) -> None:
-        if not isinstance(event, UpdateMessageReactions):
-            return
+        try:
+            if not isinstance(event, UpdateMessageReactions):
+                return
 
-        if not self.get("state", False):
-            return
+            if not self.get("state", False):
+                return
 
-        if (
-            not hasattr(event, "reactions")
-            or not hasattr(event.reactions, "recent_reactions")
-            or not isinstance(event.reactions.recent_reactions, (list, set, tuple))
-            or not any(i.unread for i in event.reactions.recent_reactions)
-        ):
-            return
+            if (
+                not hasattr(event, "reactions")
+                or not hasattr(event.reactions, "recent_reactions")
+                or not isinstance(event.reactions.recent_reactions, (list, set, tuple))
+                or not any(i.unread for i in event.reactions.recent_reactions)
+            ):
+                return
 
-        await self._client(ReadReactionsRequest(event.peer))
-        logger.debug(f"Read reaction in {event.peer}")
+            self._flood_protect = list(filter(lambda x: x > time.time(), self._flood_protect))
+
+            if len(self._flood_protect) > self._threshold:
+                self._queue[utils.get_chat_id(event)] = time.time() + 15
+                logger.debug(f"Flood protect triggered, chat {event} added to queue")
+                return
+
+            self._flood_protect += [round(time.time()) + self._flood_protect_sample]
+
+            await self._client(ReadReactionsRequest(event.peer))
+            logger.debug(f"Read reaction in {event.peer}")
+        except Exception:
+            logger.exception("Caught exception in Emotionless")
